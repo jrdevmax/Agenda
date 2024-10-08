5b2f30cc7a13f5550b47ad4690dc26fd47847e3b
jrdevmax <jr.anegocios@gmail.com>
2024-10-08 20:13:17 -0300

    config
    configuração de diretório

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include_once 'template/header.php' ?>
+
+<h1>Testando pagina</h1>
+<?php include_once 'template/footer.php' ?>
---
+++ template/header.php
@@ -0,0 +1,15 @@
+<?php include_once 'config/url.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de Contatos</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div>
+            <a href="" class="logo"></a>
+        </div>
+    </header>
