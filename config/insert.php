af1d85b771c61c1ecfd47c992f595671fdfac51f
jrdevmax <jr.anegocios@gmail.com>
2025-04-06 18:10:50 -0300

    Update insert.php
    Verification if contacts exist

--- config/insert.php
+++ config/insert.php
@@ -3,19 +3,26 @@
     include_once ("url.php");
 
     $data = $_POST;
-
+    
     if(!empty($data)){
         if($data["type"]==="insert"){
             $name = $data["name"];
             $phone = $data["phone"];
             $address = $data["address"];
             $query = "INSERT INTO contact (name, phone, address) VALUES (:name, :phone, :address)";
+            
+            $verifica = "SELECT * FROM contact WHERE id = :id, name = :name, phone = :phone, address = :address";
+            if($verifica == true){
+                echo "Contato já existe <br>";
+                header("location:../create.php");
         
-            $stmt = $con ->prepare($query);
-            $stmt ->bindParam(":name",$name);
-            $stmt ->bindParam(":phone",$phone);
-            $stmt ->bindParam(":address",$address);
-            $stmt -> execute();
+            }else{
+                $stmt = $con ->prepare($query);
+                $stmt ->bindParam(":name",$name);
+                $stmt ->bindParam(":phone",$phone);
+                $stmt ->bindParam(":address",$address);
+                $stmt -> execute();
+            }
         }
         header("location:../contacts.php");
     }
